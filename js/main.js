6dc5aa0d9d902f1180a7d1d3ec08ad522b4fdb57
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2025-05-21 18:24:34 +0100

    created a public and private folder

--- js/main.js
+++
@@ -1,2 +0,0 @@
-// Futuras interações com JavaScript, como filtros ou pesquisas
-console.log("TutorMatch loaded!");
--- js/messages.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const msg = document.querySelector('.flash-message');
-    if (msg) {
-      setTimeout(() => {
-        msg.style.display = 'none';
-      }, 3500);
-    }
-  });
-  
